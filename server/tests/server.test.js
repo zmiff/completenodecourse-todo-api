0e3f3ea204057e50a1b4fe38d5d48f8a66b4edf5
Ugur Tamer <UgurTamer1@Ugurs-MacBook-Air.local>
2017-11-07 15:47:28 +0100

    ADD test for DELETE /users/me/token

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -303,3 +303,18 @@ describe('POST /users/login',(done)=>{
       })
   })
 });
+
+describe('DELETE /users/me/token',()=>{
+  it('should remove authtoken on logout',(done)=>{
+    request(app)
+    .delete('/users/me/token')
+    .set('x-auth', users[0].tokens[0].token)
+    .expect(200)
+    .end((err,res)=>{
+      User.findById(users[0]._id).then((user)=>{
+        expect(user.tokens.length).toBe(0)
+        done();
+      }).catch((e)=>done(e))
+    })
+  })
+})
